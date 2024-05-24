37957e8f0da3d830c37e0a28997fadbd08726ade
07sumit1002 <sumitkumar101984@gmail.com>
2024-05-24 22:19:09 +0530

    Create precache-manifest.4d321afb3834f56518f3c4fd18653c50.js

---
+++ build/precache-manifest.4d321afb3834f56518f3c4fd18653c50.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960ca83740be643dda0e62b62ee4a55d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0527d3e6367d2c6aa07c",
+    "url": "/static/js/2.f99de23b.chunk.js"
+  },
+  {
+    "revision": "3174427fe6d8ce01797b",
+    "url": "/static/js/main.8121bb9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
